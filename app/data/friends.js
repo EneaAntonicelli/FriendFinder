a6b6a074bbd9e532a131e95afb5b16596a95fdcd
Enea <eneasecondary@gmail.com>
2018-09-01 13:24:54 -0700

    There was an issue where my origin was linking all my repositories, even with the path being specified to this one. I recreated and cloned the repo and manually moved the files back in.

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+var friendArray = [
+	{
+		name: 'Roger Pendenheimer',
+		photo: 'https://stpetersburgconferenceonworldaffairs.com/conference/wp-content/uploads/2016/08/Sri-Sridharan-web.jpg',
+		scores: ["1","1","4","1","1","1","2","2","1","1"]
+	},
+	{
+		name: 'Collin \'The Hitman\' Chew',
+		photo: 'http://www.eng.nus.edu.sg/cee/people/images/DCC/ChuaChongKheng-LTA.jpg',
+		scores: ["3","1","3","3","3","1","2","3","3","1"]
+	},
+	{
+		name: 'Elaine Haebalagi',
+		photo: 'https://pbs.twimg.com/media/C3V41ozVUAIuyup.jpg',
+		scores: ["5","5","4","4","5","5","5","5","4","5"]
+    },
+    {
+		name: 'Jordan Shnitzenweiner',
+		photo: 'https://www.nowplayingutah.com/wp-content/uploads/sites/www.nowplayingutah.com/images/2017/12/event-featured-epaniza-1514391103-400x398.jpeg',
+		scores: ["5","5","4","4","5","5","5","5","4","5"]
+	}
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendArray;
