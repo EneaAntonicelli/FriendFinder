18c604b09fa129e7f65763aead3bcd8708887702
Enea <eneasecondary@gmail.com>
2018-09-01 23:25:30 -0700

    Added the first question but am stuck on the modal trigger

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,21 +7,34 @@ module.exports = function(app) {
       });
 };
 
-app.post('/api/friends', function (req, res) {
-			
-    var newName = req.body.name;
-    var newPhoto = req.body.photo;
-    var newScores = req.body.scores;
-
-    friends.push(req.body);
-
-    var bestmatch = FindBestFriend(newName, newPhoto, newScores);
+app.post('/api/friends', function(req, res) {
     
-        var str = "{" + '"' + "name" + '"' + ": " + '"' + friendsData[bestmatch].name + '"' + "," + '"' + "photo" + '"' + ": " + '"' + friendsData[bestmatch].photo + '"' + "}";
-
-     res.contentType('application/json');
+    var userInput = req.body;
+    var userResponses = userInput.scores;
     
-     var personJSON = JSON.stringify(str);
-     res.send(personJSON);
-     
+
+
+    var matchName = '';
+    var matchPhoto = '';
+    var totalDifference = 10000; 
+
+    for (var i = 0; i < friends.length; i++) {
+    
+        var diff = 0;
+        for (var j = 0; j < userResponses.length; j++) {
+            diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+        }
+        
+        if (diff < totalDifference) {
+
+            totalDifference = diff;
+            matchName = friends[i].name;
+            matchPhoto = friends[i].photo;
+        }
+    }
+
+    friends.push(userInput);
+
+
+    res.json({status: 'OK', matchName: matchName, matchPhoto: matchPhoto});
 });
