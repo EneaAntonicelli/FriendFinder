81663dc219bcc0fe54d495ca0e2b970c0efd3d4e
Enea <eneasecondary@gmail.com>
2018-09-08 00:42:45 -0700

    Changed the javascript file pretty dramatically. Have the code snippet for validating everything but now the modal is running on the first question's completion. Unclear as to why.

--- app/public/assets/js/javascript.js
+++ app/public/assets/js/javascript.js
@@ -3,53 +3,44 @@ $(document).ready(function() {
 	$("#submit").click(function(){
 		event.preventDefault();
 		
-		if ( 	$("#nameInput").val() == '' || 
-				$("#imageInput").val() == '' || 
-				$("#question1").val() === '' || 
-				$("#question2").val() === '' || 
-				$("#question3").val() === '' || 
-				$("#question4").val() === '' || 
-				$("#question5").val() === '' || 
-				$("#question6").val() === '' || 
-				$("#question7").val() === '' || 
-				$("#question8").val() === '' || 
-				$("#question9").val() === '' || 
-				$("#question10").val() === ''
-			){
+		$('select.required').each(function(){
+			var selected = parseInt($(this).find(":selected").val());
+			if( !(selected > 0) ) {
 				alert("Please fill out all questions!");
 				return false;
-			}	
-			
-		else {
-			var newUser = {
-				name: $("#nameInput").val().trim(),
-				photo: $("#imageInput").val().trim(),
-				scores: [
-					$("#question1").val(),
-					$("#question2").val(),
-					$("#question3").val(),
-					$("#question4").val(),
-					$("#question5").val(),
-					$("#question6").val(),
-					$("#question7").val(),
-					$("#question8").val(),
-					$("#question9").val(),
-					$("#question10").val()
-				]
-			};
-			var currentURL = window.location.origin;
-			
-			$.post(currentURL + "/api/friends", newUser, function(data) {
-				console.log(data);
-				$('.modal').modal({
-					dismissible: true
+			}else {
+				var newUser = {
+					name: $("#nameInput").val().trim(),
+					photo: $("#imageInput").val().trim(),
+					scores: [
+						$("#question1").val(),
+						$("#question2").val(),
+						$("#question3").val(),
+						$("#question4").val(),
+						$("#question5").val(),
+						$("#question6").val(),
+						$("#question7").val(),
+						$("#question8").val(),
+						$("#question9").val(),
+						$("#question10").val()
+					]
+				};
+				var currentURL = window.location.origin;
+				
+				$.post(currentURL + "/api/friends", newUser, function(data) {
+					console.log(data);
+					$('.modal').modal({
+						dismissible: true
+					});
+					$("#modal1").modal('open');
+					$("#modalArea").empty();
+					$("#modalArea").text(data.name);
+					$("#friendImage").attr("src", data.photo);
 				});
-				$("#modal1").modal('open');
-				$("#modalArea").empty();
-				$("#modalArea").text(data.name);
-				$("#friendImage").attr("src", data.photo);
-			});
-		}
+			}
+		});
+
+			
 
 	}); // END OF FUNCTION
 
